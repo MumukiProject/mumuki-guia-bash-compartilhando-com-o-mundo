5a7f3ac94854c19115953d52b08c44b62c05d613
franco@mumuki.org <franco@mumuki.org>
2022-10-31 23:05:52 +0000

    Mumuki Export on 2022-10-31 23:05:52 +0000

--- 00003_Empurrando as alterações/extra.sh
+++ 00003_Empurrando as alterações/extra.sh
@@ -1,12 +1,12 @@
-mkdir libro-maravilloso
-cd libro-maravilloso
+mkdir livro-maravilhoso
+cd livro-maravilhoso
 git init .
 touch capitulo1.txt
-echo "Había un avestruz." >> capitulo1.txt
+Fato "Havia um avestruz." >> capitulo1.txt
 git add capitulo1.txt
-git commit -m "Primer capítulo"
+git commit -m "Primeiro capítulo"
 touch capitulo2.txt
-echo "Una historia" >> capitulo2.txt
+echo "Uma história" >> capitulo2.txt
 git add capitulo2.txt
 git commit -m "Segundo capítulo"
 git remote add origin http://otra-pc/foo/bar
